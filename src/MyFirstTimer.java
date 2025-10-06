f20b9f971f29e63bd58fe077fe1ad5cc0b466fea
Daphne Santos <d_santos6@u.pacific.edu>
2025-10-05 23:51:27 -0700

    A timer needs to know how long it is counting down from, it needs to know seconds, and it needs to know when to start.

--- src/MyFirstTimer.java
+++ src/MyFirstTimer.java
@@ -1,20 +1,34 @@
 import acm.graphics.*;
 import acm.program.*;
+import javax.swing.*;
+import java.awt.event.*;
 
 public class MyFirstTimer extends GraphicsProgram {
 	public static final int PROGRAM_HEIGHT = 600;
 	public static final int PROGRAM_WIDTH = 800;
 	public static final int MAX_STEPS = 20;
 	private GLabel myLabel;
-
+	private int numTimes;
+	
 	public void init() {
 		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
 		requestFocus();
 	}
 	
+	public void actionPerformed(ActionEvent e) {
+		myLabel.move(5, 0);
+	}
+	
 	public void run() {
-		myLabel = new GLabel("# of times called?", 0, 100);
+		numTimes = 0;
+		Timer t = new Timer(1000, this);
+		if(numTimes == 10) {
+			t.stop();
+		}
+		t.setInitialDelay(10);
+		myLabel = new GLabel("times called?" + numTimes);
 		add(myLabel);
+		t.start();
 	}
 	
 	public static void main(String[] args) {
